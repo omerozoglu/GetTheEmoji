14d8de9ab1fe5aae40a39d7115246c40d3093003
omerozoglu <omerozoglu.1@gmail.com>
2020-03-23 13:23:58 +0300

    feat(components): Create navbar, sidenav, body and footer components.

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MainComponent } from "../main/main.component";
+import { NavigationBarComponent } from "./components/navigation-bar/navigation-bar.component";
+import { SideNavigationComponent } from "./components/side-navigation/side-navigation.component";
+import { BodyComponent } from "./components/body/body.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [
+    MainComponent,
+    NavigationBarComponent,
+    SideNavigationComponent,
+    BodyComponent,
+    FooterComponent
+  ],
   imports: [CommonModule],
   exports: [MainComponent]
 })
