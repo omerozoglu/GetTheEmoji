5d6103930afb640399135e84a6d48faf20486ad5
omerozoglu <omerozoglu.1@gmail.com>
2020-03-25 15:46:36 +0300

    feat(carousel): Add *ngFor and setting

--- src/app/main/components/side-navigation/side-navigation.component.ts
+++ src/app/main/components/side-navigation/side-navigation.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-side-navigation',
-  templateUrl: './side-navigation.component.html',
-  styleUrls: ['./side-navigation.component.scss']
+  selector: "app-side-navigation",
+  templateUrl: "./side-navigation.component.html",
+  styleUrls: ["./side-navigation.component.scss"]
 })
 export class SideNavigationComponent implements OnInit {
+  src1: any = [
+    { src: "../../../assets/emoji1.jpg", alt: "a" },
+    { src: "../../../assets/emoji2.jpg", alt: "a" }
+  ];
+  src2: any = [
+    { src: "../../../assets/emoji3.jpg", alt: "a" },
+    { src: "../../../assets/emoji4.jpg", alt: "a" },
+    { src: "../../../assets/emoji5.jpg", alt: "a" },
+    { src: "../../../assets/emoji6.jpg", alt: "a" }
+  ];
+  src3: any = [
+    { src: "../../../assets/emoji7.jpg", alt: "a" },
+    { src: "../../../assets/emoji8.jpg", alt: "a" },
+    { src: "../../../assets/emoji9.jpg", alt: "a" }
+  ];
+  carousels: any = [this.src1, this.src2, this.src3];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
