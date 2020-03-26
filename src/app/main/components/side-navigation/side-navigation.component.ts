43ab70b81752facb37c0adf60ea3668f42a765f1
omerozoglu <omerozoglu.1@gmail.com>
2020-03-26 21:04:18 +0300

    feat(bind): child to parent data binding

--- src/app/main/components/side-navigation/side-navigation.component.ts
+++ src/app/main/components/side-navigation/side-navigation.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./side-navigation.component.scss"]
 })
 export class SideNavigationComponent implements OnInit {
+  currentSlideID: string;
+
   src1: any = [
     { src: "../../../assets/emoji1.jpg", alt: "a" },
     { src: "../../../assets/emoji2.jpg", alt: "a" }
@@ -17,13 +19,27 @@ export class SideNavigationComponent implements OnInit {
     { src: "../../../assets/emoji6.jpg", alt: "a" }
   ];
   src3: any = [
-    { src: "../../../assets/emoji7.jpg", alt: "a" },
-    { src: "../../../assets/emoji8.jpg", alt: "a" },
-    { src: "../../../assets/emoji9.jpg", alt: "a" }
+    {
+      src: "../../../assets/hands/icons8-left-facing-fist-emoji-50.png",
+      alt: "a"
+    },
+    {
+      src:
+        "../../../assets/hands/icons8-backhand-index-pointing-right-emoji-50.png",
+      alt: "a"
+    },
+    {
+      src: "../../../assets/hands/icons8-nikita-clapping-hands-emoji-50.png",
+      alt: "a"
+    }
   ];
   carousels: any = [this.src1, this.src2, this.src3];
 
   constructor() {}
 
   ngOnInit() {}
+
+  getSlideId($event) {
+    this.currentSlideID = $event;
+  }
 }
